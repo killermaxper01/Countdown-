b6be0520cd9d404c79be8d6ff730b8e179dfceb6
killermaxper01 <studyonlyjnv@gmail.com>
2025-12-22 21:20:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+# API key from environment variable (IMPORTANT for security)
+API_KEY = "AIzaSyAxC1aZ0zbCta0k1ebTIDvBpB-uTXjFVGg"
+
+MODEL = "gemini-2.5-flash-native-audio-preview-12-2025"
+
+@app.route("/")
+def index():
+    return render_template(
+        "index.html",
+        api_key=API_KEY,
+        model=MODEL
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
